36855e9c0fcbed7babee99df640a08f27b96b705
js0ny <json.y@outlook.com>
2024-11-01 13:05:24 +0000

    Update NuGet

--- mac/mac_setup.sh
+++ mac/mac_setup.sh
@@ -60,13 +60,14 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/p
 source $XDG_CONFIG_HOME/zsh/.zshrc
 
 echo [INFO] Setting Up dotfiles
-mkdir -p $XDG_CONFIG_HOME/conda $XDG_CONFIG_HOME/git $XDG_CONFIG_HOME/ideavim $XDG_CONFIG_HOME/markdownlint $XDG_CONFIG_HOME/pip $XDG_CONFIG_HOME/neovide $XDG_CONFIG_HOME/powershell $XDG_CONFIG_HOME/vscode
+mkdir -p $XDG_CONFIG_HOME/conda $XDG_CONFIG_HOME/git $XDG_CONFIG_HOME/ideavim $XDG_CONFIG_HOME/markdownlint $XDG_CONFIG_HOME/pip $XDG_CONFIG_HOME/neovide $XDG_CONFIG_HOME/powershell $XDG_CONFIG_HOME/vscode $XDG_CONFIG_HOME/NuGet
 mkdir -p ~/.config/zellij # Not support XDG_CONFIG_HOME but same directory
 # $DOTFILES/.config
 ln -sf $DOTFILES/.config/conda/condarc.yaml $XDG_CONFIG_HOME/conda/.condarc
 ln -sf $DOTFILES/.config/git/.gitconfig $XDG_CONFIG_HOME/git/config
 ln -sf $DOTFILES/.config/ideavim/ideavimrc.vimrc $XDG_CONFIG_HOME/ideavim/ideavimrc
 ln -sf $DOTFILES/.config/markdownlint/.markdownlint.json $XDG_CONFIG_HOME/markdownlint/markdownlint.json
+ln -sf $DOTFILES/.config/NuGet/NuGet.Config $XDG_CONFIG_HOME/NuGet/NuGet.Config
 ln -sf $DOTFILES/.config/nvim/ $XDG_CONFIG_HOME/nvim
 ln -sf $DOTFILES/.config/pip/pip.conf $XDG_CONFIG_HOME/pip/pip.conf
 ln -sf $DOTFILES/.config/zellij/config.kdl ~/.config/zellij/config.kdl
