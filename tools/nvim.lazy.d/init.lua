fdf460a547377dab137307f2cb1a4f4f9b08e169
js0ny <json.y@outlook.com>
2024-11-30 02:03:17 +0000

    feat(zsh, ipython): New symblink scripts, IPython linked with directory

--- tools/nvim.lazy.d/init.lua
+++
@@ -1,8 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-if (vim.g.vscode) then
-  print("VSCode detected, skipping lazy loading")
-  require("vscode.code")
-else
-  require("config.lazy")
-  -- require("config.lazy")
-end
