dc4647915f243d4f21024bd9b7997df780325b68
js0ny <json.y@outlook.com>
2024-12-19 19:57:47 +0000

    sync: from Linux

--- bootstrap/set_symlink_unix.bash
+++ bootstrap/set_symlink_unix.bash
@@ -11,7 +11,8 @@ mkdir -p ~/.config/zellij ~/.config/yazi ~/.config/glow
 mkdir -p $HOME/Obsidian
 # $DOTFILES/common
 ln -sf $DOTFILES/common/condarc.yaml $XDG_CONFIG_HOME/conda/.condarc
-ln -sf $DOTFILES/common/gitconfig $XDG_CONFIG_HOME/git/config
+# ln -sf $DOTFILES/common/gitconfig $XDG_CONFIG_HOME/git/config
+cp $DOTFILES/common/gitconfig.example $XDG_CONFIG_HOME/git/config
 ln -sf $DOTFILES/common/glow.yaml ~/.config/glow/config.yml
 ln -sf $DOTFILES/common/haskeline ~/.haskeline
 ln -sf $DOTFILES/common/ideavimrc $XDG_CONFIG_HOME/ideavim/ideavimrc
