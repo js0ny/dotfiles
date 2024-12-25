4657765e21a58564fa5486990c7c9f025dc85eee
js0ny <json.y@outlook.com>
2024-12-25 05:57:41 +0000

    Add lazygit keybindings

--- tools/powershell/Completions.ps1
+++ tools/powershell/Completions.ps1
@@ -5,6 +5,28 @@ function Invoke-Completion {
         'git' { Import-Module Posh-Git }
         'hugo' { hugo completion powershell | Out-String | Invoke-Expression }
         'pip' { pip completion --powershell | Out-String | Invoke-Expression }
+        'uv' { uv generate-shell-completion powershell | Out-String | Invoke-Expression }
         'wezterm' { wezterm shell-completion --shell power-shell | Out-String | Invoke-Expression }
+        'winget' {
+            # https://learn.microsoft.com/en-us/windows/package-manager/winget/tab-completion
+            Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+                param($wordToComplete, $commandAst, $cursorPosition)
+                [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+                $Local:word = $wordToComplete.Replace('"', '""')
+                $Local:ast = $commandAst.ToString().Replace('"', '""')
+                winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+                    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+                }
+            }
+        }
     }
+}
+Set-Alias "icmp" "Invoke-Completion"
+
+Register-ArgumentCompleter -CommandName Invoke-Completion -ParameterName 'command' -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    $cmds = @('docker', 'git', 'hugo', 'pip', 'uv', 'wezterm', 'winget')
+
+    $cmds | Where-Object { $_ -like "$wordToComplete*" }
 }
--- tools/powershell/readme.md
+++ tools/powershell/readme.md
@@ -20,3 +20,20 @@ This is the cross-platform PowerShell profile for PowerShell Core
 | `^a`   | To Beginning of Line | All    |
 | `^e`   | To End of Line       | All    |
 | `^[`   | To Normal Mode       | Insert |
+
+## `Get-Command` vs `which.exe` under Windows
+
+```powershell
+PS >  hyperfine "pwsh.exe -NoProfile -Command 'Get-Command which'" "pwsh.exe -NoProfile -Command 'which which'" --warmup 10
+Benchmark 1: pwsh.exe -NoProfile -Command 'Get-Command which'
+  Time (mean ± σ):     152.1 ms ±   1.3 ms    [User: 112.2 ms, System: 89.3 ms]
+  Range (min … max):   150.0 ms … 155.3 ms    18 runs
+
+Benchmark 2: pwsh.exe -NoProfile -Command 'which which'
+  Time (mean ± σ):     153.7 ms ±   6.4 ms    [User: 126.7 ms, System: 101.9 ms]
+  Range (min … max):   147.8 ms … 169.5 ms    19 runs
+
+Summary
+  pwsh.exe -NoProfile -Command 'Get-Command which' ran
+    1.01 ± 0.04 times faster than pwsh.exe -NoProfile -Command 'which which'
+```
