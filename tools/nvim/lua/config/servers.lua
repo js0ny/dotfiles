006947064134f331260361bb193a46ae093bb514
js0ny <json.y@outlook.com>
2025-01-26 14:00:07 +0000

    feat(nvim): Apperance and Cursor Movement

--- tools/nvim/lua/config/servers.lua
+++ tools/nvim/lua/config/servers.lua
@@ -3,15 +3,30 @@
 --- for available server and name
 local M = {}
 M.servers = {
+  "ast_grep",
+  "bashls", -- Bash
   "clangd", -- C/C++
+  "cssls", -- CSS
+  "denols", -- Deno
   -- "cmake",                   -- CMake
   "eslint", -- JavaScript
+  "gopls", -- Go
+  "hls", -- Haskell
   "html", -- HTML
+  "jsonls", -- JSON
   "lua_ls", -- Lua
+  "markdown_oxide", -- Markdown
   "omnisharp", -- C# & F#
   "powershell_es", -- PowerShell
   "pyright", -- Python
+  "rust_analyzer", -- Rust
+  "svelte", -- Svelte
+  "svlangserver", -- SystemVerilog
+  "tailwindcss", -- TailwindCSS
+  "taplo", -- TOML
+  "ts_ls", -- TypeScript
   "vimls", -- vimscript
+  "yamlls", -- YAML
 }
 
 M.server_config = {
