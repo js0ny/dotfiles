0c2cfd4baad3d64abcf3cc35a59f801ade79a334
js0ny <json.y@outlook.com>
2025-01-26 23:47:33 +0000

    feat(starship): Powerline prompt for all shells

--- tools/powershell/Prompt.ps1
+++ tools/powershell/Prompt.ps1
@@ -2,7 +2,6 @@
 # Date: 2024-12-01
 # Author: js0ny
 # Use starship to set prompt
-$ENV:STARSHIP_CONFIG = Join-Path $DOTFILES "tools" "starship" "starship_pwsh.toml"
 
 Invoke-Expression (&starship init powershell)
 
