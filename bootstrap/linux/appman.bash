f015467b1b16807193b6ddd99876f4d62c7c6230
js0ny <json.y@outlook.com>
2025-02-23 21:17:49 +0000

    fix(bootstrap-linux): Fix appman setup

--- bootstrap/linux/appman.bash
+++ bootstrap/linux/appman.bash
@@ -2,7 +2,9 @@ echo "[INFO] Installing AppMan"
 echo "[ACTION] Type [2] to install AppMan"
 wget -q https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER && chmod a+x ./AM-INSTALLER && ./AM-INSTALLER
 
-if command -v appman >/dev/null 2>&1; then
-    appman install wezterm
-    appman install nvim
+APPMAN_PATH="$HOME/.local/bin/appman"
+
+if command -v $APPMAN_PATH >/dev/null 2>&1; then
+    $APPMAN_PATH install wezterm
+    $APPMAN_PATH install nvim
 fi
