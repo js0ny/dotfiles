f6c8ed920d85a5b55e9b2b636d3bfbd780c3c6c1
js0ny <json.y@outlook.com>
2025-03-02 13:28:19 +0000

    minor fix

--- tools/nvim/lua/plugins/misc.lua
+++ tools/nvim/lua/plugins/misc.lua
@@ -33,4 +33,21 @@ return {
   { import = "plugins.mod.which-keys-nvim" },
   { import = "plugins.mod.copilot-lua" },
   { import = "plugins.mod.avante-nvim" },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      -- "ibhagwan/fzf-lua",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      -- configuration goes here
+    },
+  },
+  {
+    "3rd/image.nvim",
+    opts = {},
+  },
 }
--- tools/nvim/lua/plugins/mod/avante-nvim.lua
+++ tools/nvim/lua/plugins/mod/avante-nvim.lua
@@ -2,16 +2,16 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = false,   -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
     -- add any opts here
     -- for example
     provider = "openai",
     openai = {
       endpoint = "https://yunwu.ai/v1",
-      model = "claude-3-5-sonnet-latest",   -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000,                      -- timeout in milliseconds
-      temperature = 0,                      -- adjust if needed
+      model = "claude-3-7-sonnet-20250219", -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000, -- timeout in milliseconds
+      temperature = 0, -- adjust if needed
       max_tokens = 4096,
       -- reasoning_effort = "high"    -- only supported for "o" models
     },
@@ -25,10 +25,10 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "nvim-telescope/telescope.nvim",   -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp",                -- autocompletion for avante commands and mentions
-    "nvim-tree/nvim-web-devicons",     -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua",          -- for providers='copilot'
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
