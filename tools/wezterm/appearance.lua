a78096e6a45cd57becaf6f721e7f81164a2a3840
js0ny <json.y@outlook.com>
2025-03-03 15:40:48 +0000

    feat(theme): Light theme to Rose Pine Dawn

--- tools/wezterm/appearance.lua
+++ tools/wezterm/appearance.lua
@@ -18,7 +18,7 @@ local function scheme_for_appearance(appearance)
     return "Catppuccin Mocha"
   else
     print("Light")
-    return "Catppuccin Latte"
+    return "rose-pine-dawn"
   end
 end
 
