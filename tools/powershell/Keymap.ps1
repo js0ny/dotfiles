418ae4cc636f6d73c25f80bd2cbcc7365773f395
js0ny <json.y@outlook.com>
2025-03-20 20:14:21 +0000

    chore(keymaps): Standalone keymaps for Colemak hnei

--- tools/powershell/Keymap.ps1
+++ tools/powershell/Keymap.ps1
@@ -9,21 +9,6 @@ Set-PSReadLineOption -PredictionViewStyle InlineView
 Set-PSReadLineOption -PredictionSource    HistoryAndPlugin
 # Set-PSReadLineOption -ContinuationPrompt  "`e[36m CR > " # Use Starship instead
 # PSReadLineKeyHandlers
-## Colemak hnei
-Set-PSReadLineKeyHandler -Chord "n" -Function NextHistory -ViMode Command
-Set-PSReadLineKeyHandler -Chord "e" -Function PreviousHistory -ViMode Command
-Set-PSReadLineKeyHandler -Chord "i" -Function ViForwardChar -ViMode Command
-## Similar position to [i] in QWERTY
-Set-PSReadLineKeyHandler -Chord "l" -Function ViInsertMode -ViMode Command
-Set-PSReadlineKeyHandler -Chord "L" -Function ViInsertAtBegining -ViMode Command
-## Ne{[k]s}t
-Set-PSReadLineKeyHandler -Chord "k" -Function RepeatSearch -ViMode Command
-Set-PSReadLineKeyHandler -Chord "K" -Function RepeatSearchBackward -ViMode Command
-## [j]ump
-Set-PSReadLineKeyHandler -Chord "j" -Function NextWordEnd -ViMode Command
-Set-PSReadLineKeyHandler -Chord "J" -Function ViEndOfGlob -ViMode Command
-## Use N to Join
-Set-PSReadLineKeyHandler -Chord "N" -Function ViJoinLines -ViMode Command
 Set-PSReadLineKeyHandler -Chord "Control+Oem4" -Function ViCommandMode -ViMode Insert # ^[ to Escape
 Set-PSReadLineKeyHandler -Chord "Ctrl+a"	   -Function BeginningOfLine
 Set-PSReadLineKeyHandler -Chord "Ctrl+e"	   -Function EndOfLine
@@ -38,5 +23,9 @@ Set-PSReadLineKeyHandler -Chord "Alt+Backspace" -Function BackwardKillLine
 Set-PSReadLineKeyHandler -Chord "Ctrl+Delete" -Function KillWord
 Set-PSReadLineKeyHandler -Chord "Ctrl+Shift+Delete" -Function KillLine
 Set-PSReadLineKeyHandler -Chord "Alt+Delete" -Function KillLine
+Set-PSReadLineKeyHandler -Chord "Ctrl+c"     -Function CopyOrCancelLine  -ViMode Insert
+Set-PSReadLineKeyHandler -Chord "Ctrl+c"     -Function CopyOrCancelLine  -ViMode Command
 ## Use <Tab> to Invoke MenuComplete
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+. $(Join-Path $DOTFILES "tools" "powershell" "Keymap" "Colemak.ps1")
