1d1371be2b1ec1453846a2f987b45b79fc4d472e
whoami <whoami@example.com>
2025-08-17 17:23:51 +0800

    nix-darwin

---
+++ nix/hosts/macbook/default.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+{
+  # Managed by Determinate
+  nix.enable = false;
+  system.stateVersion = 6;
+  system.primaryUser = "js0ny";
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    age
+    aichat
+    bat
+    btop
+    cmake
+    coreutils
+    curlie
+    dos2unix
+    duf
+    dust
+    duti
+    fastfetch
+    fd
+    ffmpeg
+    fish
+    fzf
+    delta
+    glow
+    go
+    hyperfine
+    imagemagick
+    just
+    lazygit
+    less
+    lsd
+    neovim
+    nushell
+    pandoc
+    procs
+    pnpm
+    ripgrep
+    ripgrep-all
+    starship
+    tlrc
+    tmux
+    uv
+    zoxide
+  ];
+
+  programs.zsh.enable = true;
+
+}
