0051a163c37a5053c01dc71f13ca31ee43a4eefa
js0ny <json.y@outlook.com>
2025-09-27 02:25:06 +0100

    breaking: Remove setup script and use chezmoi

--- tools/emacs.d/lisp/init-keymaps.el
+++
@@ -1,8 +0,0 @@
-;;; init-keymaps.el
-
-;; Once this is defined, `M-x open-init-file` will open this file
-(defun open-org-config()
-  (interactive) ;; Mark function as `interactive` that allows user to access by M-x open-init-file RET
-  (find-file  "~/.dotfiles/tools/emacs.d/lisp/init-org.el"))
-
-(provide 'init-keymaps)
--- tools/emacs.d/lisp/init-writer.el
+++
@@ -1,8 +0,0 @@
-
-;; TODO
-(use-package flyspell)
-
-(use-package olivetti
-  :hook (olivetti-mode . org-mode))
-
-(provide 'init-writer)
