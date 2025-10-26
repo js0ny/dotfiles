9605a62e9cead10c5844c6ed6376ce1ae5e34d8d
js0ny <contact@js0ny.net>
2025-10-26 01:43:13 +0100

    minor fix

--- nixcfgs/modules/home/dev/verilog.nix
+++ nixcfgs/modules/home/dev/verilog.nix
@@ -1,9 +1,8 @@
-{pkgs}: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
     verilator # Formatter
     iverilog # Simulator: Icarus Verilog
     gtkwave # Waveform Viewer
-    vimPlugins.nvim-treesitter-parsers.verilog
   ];
 
   programs.zed-editor.extensions = ["verilog"];
