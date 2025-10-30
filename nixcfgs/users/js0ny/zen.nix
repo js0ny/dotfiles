1bbdd77d5ceda9167ac3840609743eaa6aad9779
js0ny <contact@js0ny.net>
2025-10-30 00:04:59 +0000

    nix-darwin home-manager

--- nixcfgs/users/js0ny/zen.nix
+++ nixcfgs/users/js0ny/zen.nix
@@ -1,12 +1,19 @@
 {...}: {
   imports = [
     ./default.nix
-    ./programs/shell.nix
+    # Shell
+    ./programs/shell/zsh.nix
+    ./programs/shell/fish.nix
+
+    # IME
+    ./programs/rime.nix
+    ./programs/nvim.nix
+    ./programs/fzf.nix
+    ./programs/sdcv.nix
+
     ../../modules/home/dev/nix.nix
     ../../modules/home/programs/eza.nix
-    ../../modules/home/programs/fzf.nix
     ../../modules/home/programs/lsd.nix
-    ../../modules/home/programs/nvim.nix
     ../../modules/home/programs/starship.nix
     ../../modules/home/programs/zoxide.nix
   ];
