0ce51ec458b0f6ab47363fd603ff8974873f0ed0
js0ny <contact@js0ny.net>
2025-11-10 18:08:32 +0000

    refractor(nvidia): Keep vfio config, rollback to disable-nvidia

--- nixcfgs/modules/nixos/hardware/udev/basys3.nix
+++ nixcfgs/modules/nixos/hardware/udev/basys3.nix
@@ -1,5 +1,6 @@
 {config, ...}: {
   # Xilinx Artix-7 Basys 3
+  # See: https://www.devicekb.com/hardware/usb-vendors/vid_0403-pid_6010
   services.udev.extraRules = ''
     ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6010", MODE="0660", GROUP="dialout", SYMLINK+="basys3"
   '';
