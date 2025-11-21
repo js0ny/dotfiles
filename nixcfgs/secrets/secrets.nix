0c501f5dc34cb3b2e73ebd3d7c32ba2979cf7fc8
js0ny <contact@js0ny.net>
2025-11-21 08:51:02 +0000

    secret: update keys

--- nixcfgs/secrets/secrets.nix
+++
@@ -1,8 +0,0 @@
-let
-  defaultAge = "age1mcvqpg39t32ll684r4m2l8j0l9zag6endg0h6zjw8svkgdwc4pjqkk5fvj";
-in {
-  age.identityPaths = [
-    "~/.config/age/keys.txt"
-  ];
-  "ts.age".publicKeys = [defaultAge];
-}
