8ae34495251a98e9e77054ca6fdaa1e4b4f1b02b
js0ny <contact@js0ny.net>
2026-01-15 14:57:00 +0000

    feishin 1.2.0 config update

--- nixcfgs/users/js0ny/programs/feishin.nix
+++ nixcfgs/users/js0ny/programs/feishin.nix
@@ -16,6 +16,13 @@
       global_media_hotkeys = true;
       window_window_bar_style = "windows";
       disable_auto_updates = true;
+      lyrics = [
+        "NetEase"
+        "lrclib.net"
+      ];
+      # For window manager (hide on close)
+      window_exit_to_tray = true;
+      window_prevent_sleep_on_playback = true;
     };
   };
 in {
